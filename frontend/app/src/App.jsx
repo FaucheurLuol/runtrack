178c108587f554b868929bbc39163f92092f598e
Lucas Baretzki <lucas.baretzki@cryptonit.fr>
2026-07-02 22:52:53 +0200

    feat: ajouter les fichiers de base pour l'application React avec routage

---
+++ frontend/app/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Accueil from './pages/Accueil';
+import Inscription from './pages/Inscription';
+import Connexion from './pages/Connexion';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route path="/" element={<Accueil />} />
+                    <Route path="/inscription" element={<Inscription />} />
+                    <Route path="/connexion" element={<Connexion />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
