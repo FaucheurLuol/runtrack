c78140e6610435b619ae59cf88d80d5e213feb8e
Lucas Baretzki <lucas.baretzki@cryptonit.fr>
2026-07-03 10:40:18 +0200

    chore: supprimer les fichiers HTML et JS de la section frontend

--- frontend/js/index.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector('.menu-burger');
-const navLiens = document.querySelector('.nav-liens');
-
-burger.addEventListener('click', () => {
-    navLiens.classList.toggle('ouvert');
-});
